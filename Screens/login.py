2ed69992553594b3d5fce1c81aa0babaf02bf322
OverlordOlv <ded6roma@mail.ru>
2021-11-21 14:18:07 +0300

    Пара фиксов

--- Screens/login.py
+++ Screens/login.py
@@ -5,3 +5,10 @@ class Login(InitScreen):
 
     def __init__(self, **kwargs):
         super(Login, self).__init__(**kwargs)
+
+    def validate(self):
+        login_widget = self.ids['login']
+        password_widget = self.ids['password']
+        login = login_widget.text
+        password = password_widget.text
+
